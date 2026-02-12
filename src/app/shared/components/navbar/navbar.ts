da7957972d31d8893d32a49e3b38f02afd940a22
Ahmed Hatem Ahmed <ugs.1331@ci.suez.edu.eg>
2026-02-13 00:36:54 +0200

    add: navbar and handle the routes in the project

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './navbar.html',
   styles: ``,
 })
