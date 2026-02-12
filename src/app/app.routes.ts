2f7cb064e18e397cc48c16392ac8ee62b669dc0c
Ahmed Hatem Ahmed <ugs.1331@ci.suez.edu.eg>
2026-02-12 23:58:40 +0200

    set up the structure of the project and handle the styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Shop } from './pages/shop/shop';
+import { AiChef } from './pages/ai-chef/ai-chef';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: Home},
+    {path: 'shop', component: Shop},
+    {path: 'aiChef', component: AiChef},
+    {path: '**', component: NotFound},
+];
---
+++ src/app/pages/ai-chef/ai-chef.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-chef',
+  imports: [],
+  templateUrl: './ai-chef.html',
+  styles: ``,
+})
+export class AiChef {
+
+}
