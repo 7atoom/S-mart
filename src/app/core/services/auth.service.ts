86b0a7f80643900ddff917a9980375443838b89b
Aasem-Mohamed <aasemmo347@gmail.com>
2026-02-20 04:30:32 +0200

    added signup inputs validations and their html

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  // private readonly httpClient = inject(HttpClient)
+  // private readonly baseUrl = 'http://localhost:3000/api/auth';
+  
+
+}
--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
-  firstName = '';
-  lastName = '';
-  email = '';
-  password = '';
-  agreeTerms = false;
+  registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    lastName: new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+    email: new FormControl(null,[Validators.required, Validators.email]),
+    password: new FormControl(null,[Validators.required, Validators.pattern( /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)])
+  })
 
-  onSubmit() {
-    // TODO: implement signup
+  onSubmit(){
+    console.log(this.registerForm)
   }
 }
