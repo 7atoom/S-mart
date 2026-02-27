728f2a03ad19d1c43471214824c32ef0bdfeaaef
Ahmed Hatem Ahmed <ugs.1331@ci.suez.edu.eg>
2026-02-27 17:15:13 +0200

    feat: implement product and category loading in home component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {Hero} from './components/hero/hero';
 import {Ramadan} from './components/ramadan/ramadan';
 import {HowItWorks} from './components/how-it-works/how-it-works';
@@ -6,6 +6,8 @@ import {Categories} from './components/categories/categories';
 import {AiChefPromo} from './components/ai-chef-promo/ai-chef-promo';
 import {PopularThisWeek} from './components/popular-this-week/popular-this-week';
 import {Footer} from './components/footer/footer';
+import {ProductsService} from '../../core/services/products.service';
+import {CategoriesService} from '../../core/services/categories.service';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +15,12 @@ import {Footer} from './components/footer/footer';
   templateUrl: './home.html',
   styles: ``,
 })
-export class Home {
+export class Home implements OnInit {
+  private productsService = inject(ProductsService);
+  private categoriesService = inject(CategoriesService);
 
+  ngOnInit() {
+    this.productsService.getProducts().subscribe();
+    this.categoriesService.getCategories().subscribe();
+  }
 }
